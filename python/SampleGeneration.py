221182ebd4db184b5ea4cfb33dacfbcc266e761b
jhasanov <jhasanov@ada.edu.az>
2020-08-15 22:53:26 +0400

    sample selection - 10 samples,Rgb2Lab and CIEDE200 part is done

--- python/SampleGeneration.py
+++ python/SampleGeneration.py
@@ -1,8 +1,8 @@
 import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
 import random
-#from skimage import color
+from skimage import color
+#from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
 
 def generateRandomPixels():
     """
@@ -22,6 +22,7 @@ def generateRandomPixels():
 def generatePairs():
     """ 
     For the each pixel sample, select:
+    1 with itself (for 100% closeness case)
     3 close points (up to 2 cubes away)
     3 mid close points (distance is between 3 and 6 cubes)
     3 far points (distance is far than 6 cubes: 7-32)
@@ -29,31 +30,38 @@ def generatePairs():
 
     global rgbs
 
-    sampleData = np.array([])
-    
+    arrlen = 32*32*320
+    rgbOriginal = np.zeros((32,32,320,3))
+    rgbOpposite = np.zeros((32,32,320,3))
+
     for i in range(0,32):
         for j in range(0,32):
             for k in range(0,32):
+                # First, compare with itself case:
+
+                rgbOriginal[i,j,k*10,:] = rgbs[i,j,k]
+                rgbOpposite[i,j,k*10,:] = rgbs[i,j,k]
+
                 # 2, 6 and 31 cubes for close, min and far, correspondingly 
-                dist_arr = [0,2,6,31]
+                dist_arr = [0,2,8,31]
                 for distIdx in range(1,4):
                     # The range can be on a negative of positive side:
                     #
                     #  _(i-6)________(i-2)___i___(i+2)________(i+6)_
                     #   maxIneg     minIneg      minIpos     maxIpos
                     #
-                    minIneg = max(0,i - dist_arr[distIdx-1])
-                    minIpos = min(31,i + dist_arr[distIdx-1])
+                    minIneg = max(0,i - (dist_arr[distIdx-1]+1))
+                    minIpos = min(31,i + (dist_arr[distIdx-1]+1))
                     maxIneg = max(0,i - dist_arr[distIdx-1] - dist_arr[distIdx])
                     maxIpos = min(31,i + dist_arr[distIdx-1] + dist_arr[distIdx])
 
-                    minJneg = max(0,j - dist_arr[distIdx-1])
-                    minJpos = min(31,j + dist_arr[distIdx-1])
+                    minJneg = max(0,j - (dist_arr[distIdx-1]+1))
+                    minJpos = min(31,j + (dist_arr[distIdx-1]+1))
                     maxJneg = max(0,j - dist_arr[distIdx-1] - dist_arr[distIdx])
                     maxJpos = min(31,j + dist_arr[distIdx-1] + dist_arr[distIdx])
 
-                    minKneg = max(0,k - dist_arr[distIdx-1])
-                    minKpos = min(31,k + dist_arr[distIdx-1])
+                    minKneg = max(0,k - (dist_arr[distIdx-1]+1))
+                    minKpos = min(31,k + (dist_arr[distIdx-1]+1))
                     maxKneg = max(0,k - dist_arr[distIdx-1] - dist_arr[distIdx])
                     maxKpos = min(31,k + dist_arr[distIdx-1] + dist_arr[distIdx])
 
@@ -89,15 +97,22 @@ def generatePairs():
                         else: # get from positive part
                             xk = random.randint(minKpos,maxKpos)
 
-                        if (sampleData.size < 6): # for the first time
-                            sampleData = np.hstack((rgbs[i,j,k],rgbs[xi,xj,xk]))
-                        else:
-                            x = np.hstack((rgbs[i,j,k],rgbs[xi,xj,xk]))
-                            sampleData = np.vstack((sampleData,x))
+                        rgbOriginal[i,j,k*10 + 1 + (distIdx-1)*3 + x,:] = rgbs[i,j,k]
+                        rgbOpposite[i,j,k*10 + 1 + (distIdx-1)*3 + x,:] = rgbs[xi,xj,xk]
 
+    lab1 = color.rgb2lab(rgbOriginal/255.0)
+    lab2 = color.rgb2lab(rgbOpposite/255.0)
 
+    sampleData = np.hstack((
+        rgbOriginal.reshape(arrlen,3),
+        rgbOpposite.reshape(arrlen,3),
+        lab1.reshape(arrlen,3),
+        lab2.reshape(arrlen,3),
+        color.deltaE_ciede2000(lab1,lab2).reshape(arrlen,1)))
     np.savetxt('sampleData.csv', sampleData, delimiter=',',fmt="%s")
     print('Done')
+    #plt.scatter(lab1[:,:,:,0], lab1[:,:,:,1], lab1[:,:,:,2], c= 'red')
+
 
 rgbs = generateRandomPixels()
 generatePairs()
@@ -110,7 +125,7 @@ generatePairs()
 #fig = plt.figure()
 #ax = fig.add_subplot(32,32,32, projection='3d')
 #plt.grid(b=True)
-#plt.scatter(rgbs[0:5,0:5,0:5,0], rgbs[0:5,0:5,0:5,1], rgbs[0:5,0:5,0:5,2], c= 'red')
+#plt.scatter(lab1[0:5,0:5,0:5,0], rgbs[0:5,0:5,0:5,1], rgbs[0:5,0:5,0:5,2], c= 'red')
 #plt.xlim(0,24)
 #plt.xticks(np.arange(0, 40, 8))
 #plt.yticks(np.arange(0, 40, 8))
